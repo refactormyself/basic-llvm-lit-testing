54ff6242a80a1cafda697fce6b4a560d31fd01e4
Saheed <refactormyself@gmail.com>
2022-06-10 14:26:48 +0200

    Remove unnecessary configurations

--- build-proj.sh
+++ build-proj.sh
@@ -1,26 +1,13 @@
 #! /usr/bin/env bash
 
 CURR_DIR=$PWD
-LLVM_BUILD="/DATA/DEV/Compilers/llvm/build"
-LLVM_INSTALL="/DATA/DEV/Compilers/llvm/install"
-# APP_INSTALL="$CURR_DIR/bin"
 
-# rm -rf $APP_INSTALL
-# rm -rf build
-
-# mkdir $APP_INSTALL
+rm -rf build
 mkdir build
 cd build
 
-export CC=$LLVM_INSTALL/bin/clang
-export CXX=$LLVM_INSTALL/bin/clang++
-
-cmake -G Ninja .. \
-	-DLLVM_DIR=$LLVM_INSTALL/lib/cmake/llvm \
-	-DLLVM_EXTERNAL_LIT=$LLVM_BUILD/bin/llvm-lit 
-    # -DCMAKE_INSTALL_PREFIX=$APP_INSTALL 
-
-cmake --build . #--target install
+cmake ..
+cmake --build . 
 
 echo
 ./lit-cpp-add
